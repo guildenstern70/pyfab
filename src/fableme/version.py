4ac8b80a898e8a4fe3f5f8ff601f3740f4a3c0b4
guildenstern70 <alessiosaltarin@gmail.com>
2015-07-01 12:12:43 +0200

    Restored version

---
+++ src/fableme/version.py
@@ -0,0 +1,17 @@
+"""
+ FABLE-O-MATIC
+ A LittleLite Web Application
+
+ version.py
+
+"""
+
+# Global variables
+VERSION_MAJOR = 0
+VERSION_MINOR = 9
+VERSION_BUILD = 9217
+
+# Global methods
+def version():
+    """ FableMe version """
+    return str(VERSION_MAJOR)+"."+str(VERSION_MINOR)+"."+str(VERSION_BUILD)
